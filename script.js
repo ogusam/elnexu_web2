4db5c5a54ac46954c404073027eb8e65bd7dce97
ogusam <ogusam32@gmail.com>
2024-05-13 06:40:09 +0200

    second commit

--- script.js
+++ script.js
@@ -11,3 +11,31 @@ window.onscroll=()=>{
     nav.classList.remove('active');
  
 }
+
+
+let slider=tns({
+    container : ".my-slider",
+    "slideBy" : "1",
+    "speed" :2600,
+    "nav" : false,
+    autoplay: true,
+    controls: false,
+    autoplayButtonOutput :false,
+    responsive: {
+        1600: {
+            items:5,
+            gutter : 20
+        },
+        1024:{
+            items:4,
+            gutter :20
+        },
+        768:{
+            items: 3,
+            gutter: 20,
+        },
+        480:{
+            items: 1
+        }
+    }
+})
